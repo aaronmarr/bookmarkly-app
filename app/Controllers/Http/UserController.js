dd99bf7e3d28045bedd2521837424273bfd85893
Aaron Marr <aaron.marr@headforwards.com>
2020-04-11 11:37:05 +0100

    Implement basic styling and content for login, signup and home pages

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -22,7 +22,7 @@ class UserController {
       await auth.attempt(email, password);
       return response.redirect('/');
     } catch (error) {
-      session.flash({loginError: 'These credentials do not work.'})
+      session.flash({ loginError: 'These credentials do not work.' })
       return response.redirect('/login');
     }
   }
